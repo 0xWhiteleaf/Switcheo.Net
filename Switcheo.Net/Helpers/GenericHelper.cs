76f21587ea24c5cff186840591404a237f7b1dc0
Noé Comte <zalir0@live.fr>
2018-11-01 18:00:58 +0100

    Adding T[] SubArray<T> extension

--- Switcheo.Net/Helpers/GenericHelper.cs
+++ Switcheo.Net/Helpers/GenericHelper.cs
@@ -1,4 +1,6 @@
-﻿namespace Switcheo.Net.Helpers
+﻿using System;
+
+namespace Switcheo.Net.Helpers
 {
     public static class GenericHelper
     {
@@ -7,6 +9,13 @@
             bool isDefault = value.Equals(default(T));
 
             return isDefault;
+        }
+
+        public static T[] SubArray<T>(this T[] data, int index, int length)
+        {
+            T[] result = new T[length];
+            Array.Copy(data, index, result, 0, length);
+            return result;
         }
     }
 }
