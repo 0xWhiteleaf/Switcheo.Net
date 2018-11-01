86503a2d3916a3d3d032158c55d0b9242d499c9c
Noé Comte <zalir0@live.fr>
2018-11-01 18:01:16 +0100

    Adding a method GetMyWalletBalances to fecth user wallet balance

---
+++ Switcheo.Net/Exceptions/RpcClientBusyNodesException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Switcheo.Net.Exceptions
+{
+    public class RpcClientBusyNodesException : Exception
+	{
+        public RpcClientBusyNodesException() { }
+
+        public RpcClientBusyNodesException(string message): base(message) { }
+    }
+}
